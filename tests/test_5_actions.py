7b2222e08409fc33d37ef403cf7a33d5f4ff62b4
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2025-04-21 17:21:18 +0200

    remove exposed_actions test as it may not reliably work

--- tests/test_5_actions.py
+++ tests/test_5_actions.py
@@ -509,72 +509,7 @@ class TestAction(TestCase):
         self.assertIsNone(affordance.description) # no doc
 
         
-    def test_7_exposed_actions(self):
-        """Test if actions can be invoked by a client"""
-        start_thing_forked(
-            thing_cls=self.thing_cls, 
-            id='test-action', 
-            done_queue=self.done_queue,
-            log_level=logging.ERROR+10, 
-            prerun_callback=replace_methods_with_actions,
-        )
-        thing = self.thing_cls(id='test-action', log_level=logging.ERROR)
-        self.client.handshake()
-
-        # thing_client = ObjectProxy('test-action', log_level=logging.ERROR) # type: TestThing
-        assert isinstance(thing.action_echo, BoundAction) # type definition
-        action_echo = ZMQAction(
-            resource=thing.action_echo.to_affordance(),
-            sync_client=self.client
-        )
-        self.assertEqual(action_echo(1), 1)
-        
-        assert isinstance(thing.action_echo_with_classmethod, BoundAction) # type definition
-        action_echo_with_classmethod = ZMQAction(
-            resource=thing.action_echo_with_classmethod.to_affordance(),
-            sync_client=self.client
-        )
-        self.assertEqual(action_echo_with_classmethod(2), 2)
-
-        assert isinstance(thing.action_echo_async, BoundAction) # type definition
-        action_echo_async = ZMQAction(
-            resource=thing.action_echo_async.to_affordance(),
-            sync_client=self.client
-        )
-        self.assertEqual(action_echo_async("string"), "string")
-
-        assert isinstance(thing.action_echo_async_with_classmethod, BoundAction) # type definition
-        action_echo_async_with_classmethod = ZMQAction(
-            resource=thing.action_echo_async_with_classmethod.to_affordance(),
-            sync_client=self.client
-        )
-        self.assertEqual(action_echo_async_with_classmethod([1, 2]), [1, 2])
-
-        assert isinstance(thing.parameterized_action, BoundAction) # type definition
-        parameterized_action = ZMQAction(
-            resource=thing.parameterized_action.to_affordance(),
-            sync_client=self.client
-        )
-        self.assertEqual(parameterized_action(arg1=1, arg2='hello', arg3=5), ['test-action', 1, 'hello', 5])
-
-        assert isinstance(thing.parameterized_action_async, BoundAction) # type definition
-        parameterized_action_async = ZMQAction(
-            resource=thing.parameterized_action_async.to_affordance(),
-            sync_client=self.client
-        )
-        self.assertEqual(parameterized_action_async(arg1=2.5, arg2='hello', arg3='foo'), ['test-action', 2.5, 'hello', 'foo'])
-
-        assert isinstance(thing.parameterized_action_without_call, BoundAction) # type definition
-        parameterized_action_without_call = ZMQAction(
-            resource=thing.parameterized_action_without_call.to_affordance(),
-            sync_client=self.client
-        )
-        with self.assertRaises(NotImplementedError) as ex:
-            parameterized_action_without_call(arg1=2, arg2='hello', arg3=5)
-        self.assertTrue(str(ex.exception).startswith("Subclasses must implement __call__"))
-        
-
-    def test_8_schema_validation(self):
+    def test_7_schema_validation(self):
         """Test if schema validation is working correctly"""
         self._test_8_json_schema_validation()
         self._test_8_pydantic_validation()
