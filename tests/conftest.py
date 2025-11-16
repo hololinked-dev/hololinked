c394b0ff63d22be14506b24c56174ef4e4df5a33
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2025-11-16 18:43:51 +0100

    remove unnecessary fixtures in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,17 @@
 """pytest configuration and shared fixtures for hololinked tests"""
 
-import asyncio
 import logging
-import sys
 
 from dataclasses import dataclass
-from typing import Generator
 from uuid import uuid4
 
 import pytest
 import zmq.asyncio
 
-from faker import Faker
-
 from hololinked.config import global_config
 from hololinked.logger import setup_logging
 from hololinked.serializers import Serializers
+from hololinked.utils import get_current_async_loop, set_global_event_loop_policy
 
 
 @dataclass
@@ -33,39 +29,18 @@ class AppIDs:
     """A thing ID"""
 
 
-@pytest.fixture(scope="session")
-def fake() -> Faker:
-    """Provide a Faker instance for generating test data."""
-    return Faker()
-
-
-@pytest.fixture()
-def event_loop() -> Generator[asyncio.AbstractEventLoop, None, None]:
-    """Create an instance of the default event loop for the test session."""
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    if sys.platform.startswith("win"):
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.set_event_loop(loop)
-    yield loop
-    loop.close()
-
-
-@pytest.fixture()
-def zmq_context() -> Generator[zmq.asyncio.Context, None, None]:
-    """Setup ZMQ context for test classes."""
-    global_config.ZMQ_CONTEXT = zmq.asyncio.Context()
-    yield global_config.ZMQ_CONTEXT
-    global_config.ZMQ_CONTEXT.term()
-
-
-@pytest.fixture()
-def setup_test_environment(zmq_context, event_loop):
+# @pytest.fixture(autouse=True, scope="module")
+def setup_test_environment():
     """Automatically setup test environment for each file"""
     # This fixture runs automatically for every test
+    set_global_event_loop_policy()
+    global_config.ZMQ_CONTEXT = zmq.asyncio.Context()
     setup_logging(log_level=logging.ERROR + 10)
     yield
     # Reset serializers after each test
     Serializers().reset()
+    global_config.ZMQ_CONTEXT.term()
+    get_current_async_loop().close()
 
 
 @pytest.fixture()
