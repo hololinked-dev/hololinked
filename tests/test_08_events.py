8e64c50992504aabf773145dbfdcc59f0d146384
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2025-11-16 12:32:22 +0100

    verify test 8

--- tests/test_08_events.py
+++ tests/test_08_events.py
@@ -4,6 +4,7 @@ from hololinked.core.events import Event, EventDispatcher
 from hololinked.core.zmq.brokers import EventPublisher
 from hololinked.td.interaction_affordance import EventAffordance
 from hololinked.logger import setup_logging
+from hololinked.utils import uuid_hex
 
 try:
     from .things import TestThing
@@ -15,15 +16,19 @@ setup_logging(log_level=logging.ERROR)
 
 @pytest.fixture(scope="module")
 def thing():
-    return TestThing(id="test-event")
+    return TestThing(id=f"test-event-{uuid_hex()}")
 
 
-def _test_dispatcher(descriptor: Event, dispatcher: EventDispatcher, thing: TestThing):
-    assert isinstance(dispatcher, EventDispatcher)  # instance access returns dispatcher
-    assert dispatcher._owner_inst is thing  # dispatcher has the owner instance
+def validate_event_dispatcher(descriptor: Event, dispatcher: EventDispatcher, thing: TestThing):
+    # instance access returns dispatcher
+    assert isinstance(dispatcher, EventDispatcher)
+    # dispatcher has the owner instance
+    assert dispatcher._owner_inst is thing
+    # event publisher and RPC server presence depends on whether the thing has been started or not
     assert (
         thing.rpc_server and thing.rpc_server.event_publisher and isinstance(dispatcher.publisher, EventPublisher)
     ) or dispatcher.publisher is None
+    # unique identifier is correctly formed, qualified by the thing ID
     assert dispatcher._unique_identifier == f"{thing._qualified_id}/{descriptor.name}"
 
 
@@ -32,7 +37,7 @@ def test_1_pure_events(thing):
     # 1. Test class-level access to event descriptor
     assert isinstance(TestThing.test_event, Event)  # class access returns descriptor
     # 2. Test instance-level access to event dispatcher which is returned by the descriptor
-    _test_dispatcher(TestThing.test_event, thing.test_event, thing)  # test dispatcher returned by descriptor
+    validate_event_dispatcher(TestThing.test_event, thing.test_event, thing)  # test dispatcher returned by descriptor
     # 3. Event with JSON schema has schema variable set
 
 
@@ -44,38 +49,24 @@ def test_2_observable_events(thing):
     assert isinstance(TestThing.observable_readonly_prop._observable_event_descriptor, Event)
 
     # 2. observable descriptors have been assigned as an attribute of the owning class
-    assert hasattr(
-        TestThing,
-        TestThing.observable_list_prop._observable_event_descriptor.name,
-    )
+    assert hasattr(TestThing, TestThing.observable_list_prop._observable_event_descriptor.name)
     assert hasattr(TestThing, TestThing.state._observable_event_descriptor.name)
-    assert hasattr(
-        TestThing,
-        TestThing.observable_readonly_prop._observable_event_descriptor.name,
-    )
+    assert hasattr(TestThing, TestThing.observable_readonly_prop._observable_event_descriptor.name)
 
     # 3. accessing those descriptors returns the event dispatcher
-    _test_dispatcher(
+    validate_event_dispatcher(
         TestThing.observable_list_prop._observable_event_descriptor,
-        getattr(
-            thing,
-            TestThing.observable_list_prop._observable_event_descriptor.name,
-            None,
-        ),
+        getattr(thing, TestThing.observable_list_prop._observable_event_descriptor.name, None),
         thing,
     )
-    _test_dispatcher(
+    validate_event_dispatcher(
         TestThing.state._observable_event_descriptor,
         getattr(thing, TestThing.state._observable_event_descriptor.name, None),
         thing,
     )
-    _test_dispatcher(
+    validate_event_dispatcher(
         TestThing.observable_readonly_prop._observable_event_descriptor,
-        getattr(
-            thing,
-            TestThing.observable_readonly_prop._observable_event_descriptor.name,
-            None,
-        ),
+        getattr(thing, TestThing.observable_readonly_prop._observable_event_descriptor.name, None),
         thing,
     )
 
