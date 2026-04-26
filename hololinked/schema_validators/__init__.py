684dd0f36f5b2014bd5d231ac2bf47e15daf9a17
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2026-04-26 10:44:28 +0200

    optimize based on DB diff

--- hololinked/schema_validators/__init__.py
+++ hololinked/schema_validators/__init__.py
@@ -1,4 +1,12 @@
-"""Validators for validating data against schemas."""
+"""
+Validators for validating data against schemas.
+
+All of properties, actions and events validate can their payload or types against the schema.
+For properties and actions, validation is carried out right before carrying out the operation.
+For events, such validation is missing on the client. For properties and actions, said validation is also missing
+on the client. This is an architectural error that needs to be fixed. The current architecture of the package leads
+to duplication of code if this is implemented as is. Therefore it has been left out by choice.
+"""
 
 from .validators import BaseSchemaValidator, JSONSchemaValidator, PydanticSchemaValidator  # noqa
 from .json_schema import JSONSchema  # noqa
