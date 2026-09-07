79023f9e071005b617ac7964978faa6fde3d1b05
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2026-09-07 20:46:28 +0200

    update test import

--- tests/test_16_protocols_mqtt.py
+++ tests/test_16_protocols_mqtt.py
@@ -13,9 +13,9 @@ from testcontainers.mqtt import (
 
 from hololinked.client import ClientFactory, ObjectProxy
 from hololinked.client.abstractions import SSE
+from hololinked.server import run
 from hololinked.server.http.server import HTTPServer
 from hololinked.server.mqtt.server import MQTTPublisher
-from hololinked.server.server import run
 from hololinked.utils import uuid_hex
 
 
